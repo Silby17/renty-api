ab1fc69c87e5194f6396497cfd193c9497fdcefb
Yosef Silberhaft <yossisilb@gmail.com>
2017-12-07 12:35:12 +0200

    initial commit with all new code

---
+++ server/server.js
@@ -0,0 +1,54 @@
+const _ = require('lodash');
+var express = require('express');
+var bodyParser = require('body-parser');
+var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
+
+var app = express();
+app.use(bodyParser.json());
+
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
+
+
+// Login Post
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send({message: 'wrong'});
+    });
+});
+
+
+app.delete('/users/logout', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('Started server on Port 3000');
+});
