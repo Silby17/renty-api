cd7886cd13d2ead0048774172e9a13ae90ee7179
Yosef Silberhaft <yossisilb@gmail.com>
2017-12-07 12:34:21 +0200

    cleaned up code

--- server/models/user.js
+++ server/models/user.js
@@ -52,7 +52,6 @@ UserSchema.methods.generateAuthToken = function () {
 
 UserSchema.methods.removeToken = function(token) {
     var user =this;
-
     return user.update({
         $pull: {
             tokens: {token}
