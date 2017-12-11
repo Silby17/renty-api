2f3e0d0effafce5da85663f3897200e9ba5b2e9e
Yosef Silberhaft <yossisilb@gmail.com>
2017-12-11 21:21:42 +0200

    added connection success log

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,5 +3,6 @@ mongoose.Promise = require('bluebird');
 
 var options = {useMongoClient: true};
 mongoose.connect('mongodb://silby:silby@ds129906.mlab.com:29906/renty', options);
+console.log('DB Connection Successful');
 
 module.exports = {mongoose};
