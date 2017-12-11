3782b3a25ce9fe16aa35c31751fa7572bd8ac029
Yosef Silberhaft <yossisilb@gmail.com>
2017-12-11 14:06:41 +0200

    initial commit

---
+++ server/models/listing.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+var ListingSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        required: true,
+        type: Number
+    },
+    imageUrl: {
+        type: String
+    },
+    _creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    }
+    /*prices: [{
+        dailyPrice: {
+            required: true,
+            type: Number
+        },
+        weeklyPrice: {
+            required: true,
+            type: Number
+        },
+        monthlyPrice: {
+            required: true,
+            type: Number
+        }
+    }],*/
+
+});
+
+var Listing = mongoose.model('Listing', ListingSchema);
+
+module.exports = {Listing};
