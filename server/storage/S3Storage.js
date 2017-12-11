325ce1e7564b0afc99846c509e952cca03600be8
Yosef Silberhaft <yossisilb@gmail.com>
2017-12-11 14:39:36 +0200

    initial commit

---
+++ server/storage/S3Storage.js
@@ -0,0 +1,28 @@
+var multer = require('multer');
+var multerS3 = require('multer-s3');
+var AWS = require('aws-sdk');
+
+AWS.config.update({region: 'eu-west-1'});
+
+var s3 = new AWS.S3({
+    Bucket: 'testing-tent-me'
+});
+
+var upload = multer({
+    storage: multerS3({
+        s3: s3,
+        acl: 'public-read',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        bucket: 'testing-tent-me/itemImages',
+        metadata: function (req, file, cb) {
+            cb(null, {fieldName: file.fieldname});
+        },
+        key: function (req, file, cb) {
+            cb(null, Date.now().toString())
+        }
+    })
+});
+
+module.exports = {AWS};
+module.exports = {s3};
+module.exports = {upload};
