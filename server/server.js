38dcc93435765260e8dc877fb1ea4398aae95a57
Yosef Silberhaft <yossisilb@gmail.com>
2017-12-12 08:18:18 +0200

    added getting all listings and logged in users listings

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
+var {mongoose} = require('./db/mongoose'); //Needed to start connection
 var {User} = require('./models/user');
 var {Listing} = require('./models/listing');
 var {authenticate} = require('./middleware/authenticate');
@@ -28,22 +29,6 @@ app.post('/users', (req, res) => {
     })
 });
 
-app.post('/listing', upload.single('myFile'), authenticate, (req, res, next) => {
-    var fileLocation = req.file.location;
-    var body = _.pick(req.body, ['title', 'category', 'description',
-    'price']);
-    var listing = new Listing(body);
-    listing._creator =  req.user._id;
-    listing.imageUrl = fileLocation;
-    listing.save().then((doc) => {
-        console.log('Created Listing: ', doc);
-        res.send(doc);
-    }, (err) => {
-        res.status(400).send(e);
-    });
-});
-
-
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
@@ -68,6 +53,39 @@ app.delete('/users/logout', authenticate, (req, res) => {
         res.status(200).send();
     }, () => {
         res.status(400).send();
+    });
+});
+
+
+app.post('/listing', upload.single('myFile'), authenticate, (req, res, next) => {
+    var fileLocation = req.file.location;
+    var body = _.pick(req.body, ['title', 'category', 'description',
+        'price']);
+    var listing = new Listing(body);
+    listing._creator =  req.user._id;
+    listing.imageUrl = fileLocation;
+    listing.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/listings', authenticate, (req, res) => {
+    Listing.find({
+        _creator: req.user._id
+    }).then((listings) => {
+        res.send(listings);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.get('/all/listings', (req, res) => {
+    Listing.find().then((listings) => {
+        res.send({listings});
+    }, (err) => {
+        res.status(400).send(err);
     });
 });
 
