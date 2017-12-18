7e31de72c0e4fa8ace7559384c332f14f3bee821
Yosef Silberhaft <yossisilb@gmail.com>
2017-12-18 19:31:26 +0200

    changed welcome msg

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send({message: "Mundo API Live!"})
+    res.send({message: "The Mundo API is Live!"})
 });
 
 
